d74596c853db9a78c274b89d141c500b9c16f648
shvam0000 <shvam0000@gmail.com>
2022-09-05 16:44:00 -0300

    :zap: add product

---
+++ pages/add-product.tsx
@@ -0,0 +1,152 @@
+import React, { useState } from 'react';
+import { Navbar } from '../components';
+import axios from 'axios';
+
+const optionsList = [
+  {
+    value: 'accessories',
+    option: 'Accessories',
+  },
+  {
+    value: 'electronics',
+    option: 'Electronics',
+  },
+  {
+    value: 'clothing',
+    option: 'Clothing',
+  },
+  {
+    value: 'furniture',
+    option: 'Furniture',
+  },
+];
+
+const AddProduct = () => {
+  const [name, setName] = useState<string>('');
+  const [price, setPrice] = useState<any>();
+  const [category, setCategory] = useState<string>('');
+  const [description, setDescription] = useState<string>('');
+  const [avatar, setAvatar] = useState<string>('');
+  const [email, setEmail] = useState<string>('shvam0000@gmail.com');
+
+  const data = {
+    name: name,
+    price: price,
+    category: category,
+    description: description,
+    avatar: avatar,
+    email: email,
+  };
+
+  const submitHandler = () => {
+    fetch(`https://upayments-studycase-api.herokuapp.com/api/products`, {
+      method: 'POST',
+      body: JSON.stringify({
+        Name: name,
+        Price: price,
+        Category: category,
+        Description: description,
+        Avatar: avatar,
+        DeveloperEmail: email,
+      }),
+      headers: {
+        Authorization: `Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJlbWFpbCI6InNodmFtMDAwMEBnbWFpbC5jb20iLCJnaXRodWIiOiJodHRwczovL2dpdGh1Yi5jb20vc2h2YW0wMDAwIiwiaWF0IjoxNjYyMzk1MjczLCJleHAiOjE2NjI4MjcyNzN9.UPDuqM1q5mnvXQGhX6_yoVAJmTjb7-DAsRhaIo7iQtA`,
+      },
+    })
+      .then((res) => {
+        console.log(res);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  };
+
+  return (
+    <div className="bg-[#ECECED] py-4 min-h-screen">
+      <Navbar />
+      <h1 className="flex justify-center items-center py-5 text-3xl font-medium">
+        Create Product
+      </h1>
+      <div className="flex justify-center">
+        <form onSubmit={submitHandler}>
+          <div>
+            <input
+              value={name}
+              onChange={(e) => setName(e.target.value)}
+              required
+              type="text"
+              placeholder="Product Name"
+              className="px-9 py-4 my-3 rounded-lg w-xl shadow-xl"
+            />
+          </div>
+          <div>
+            <textarea
+              value={description}
+              onChange={(e) => setDescription(e.target.value)}
+              required
+              placeholder="Description"
+              className="px-9 py-4 rounded-lg w-xl shadow-xl"
+              cols={24}
+              rows={4}
+            />
+          </div>
+          <div>
+            <input
+              value={avatar}
+              onChange={(e) => setAvatar(e.target.value)}
+              required
+              type="text"
+              placeholder="Image URL"
+              className="px-9 py-4 my-3 rounded-lg w-xl shadow-xl"
+            />
+          </div>
+          <div>
+            <select
+              value={category}
+              onChange={(e) => setCategory(e.target.value)}
+              className="bg-white px-24 flex py-4 my-3 rounded-lg shadow-xl"
+              name="categories">
+              <option value="" disabled defaultValue="Categories">
+                Categories
+              </option>
+              {optionsList.map((option) => (
+                <option key={option.value} value={option.value}>
+                  {option.option}
+                </option>
+              ))}
+            </select>
+          </div>
+          <div>
+            <input
+              value={price}
+              onChange={(e) => setPrice(e.target.value)}
+              required
+              type="number"
+              placeholder="Price"
+              className="px-9 py-4 my-3 rounded-lg w-xl shadow-xl"
+            />
+          </div>
+          <div>
+            <input
+              value={email}
+              onChange={(e) => setEmail(e.target.value)}
+              required
+              type="email"
+              placeholder="Developer Email"
+              className="px-9 py-4 my-3 rounded-lg w-xl shadow-xl"
+            />
+          </div>
+          <div>
+            <button
+              type="submit"
+              className="bg-white px-36 flex py-4 my-3 rounded-lg shadow-xl uppercase">
+              Submit
+            </button>
+          </div>
+        </form>
+      </div>
+    </div>
+  );
+};
+
+export default AddProduct;
--- pages/index.tsx
+++ pages/index.tsx
@@ -2,6 +2,8 @@ import { ItemCard, Navbar } from '../components';
 import axios from 'axios';
 import { useEffect, useState } from 'react';
 import { useRouter } from 'next/router';
+import { Plus } from '../utils/icons';
+import Link from 'next/link';
 
 export default function Home() {
   const router = useRouter();
@@ -23,7 +25,7 @@ export default function Home() {
   }, []);
 
   return (
-    <div className="bg-[#ECECED] py-4">
+    <div className="bg-[#ECECED] py-4 ">
       <Navbar />
       <div className="grid grid-cols-1 gap-y-10 gap-x-6 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 xl:gap-x-8 ">
         {productData.map((data, index) => (
@@ -39,6 +41,11 @@ export default function Home() {
             />
           </div>
         ))}
+      </div>
+      <div className="text-6xl bottom-0 left-0 sticky">
+        <a href="/add-product">
+          <Plus />
+        </a>
       </div>
     </div>
   );
